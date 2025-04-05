bbcd54786e7f723a51741caacc02d8970b212842
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-04-05 18:15:48 +0530

    made the changes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         service: './service.html',
         services: './services.html',
         about: './about-us.html',
-        
+        data: './data/services.json',
+
                 // Add more HTML files as needed
       }
     }
