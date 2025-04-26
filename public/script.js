1b2850e10d84d0ad5c9a4a0714e96bb39975ff01
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-04-26 19:28:12 +0530

    mad echange

--- public/script.js
+++ public/script.js
@@ -25,6 +25,66 @@ document.addEventListener('click', (e) => {
     }
 });
 
+// Function to close enquiry popup
+function closeEnquiryPopup() {
+    document.getElementById('enquiryPopup').classList.add('hidden');
+    document.body.style.overflow = 'auto'; // Restore scrolling
+}
+
+// Handle form submission
+async function handleEnquirySubmit(event) {
+    event.preventDefault();
+    let name = document.getElementById('name').value 
+    let email = document.getElementById('email').value
+    let mobile = document.getElementById('mobile').value
+    let organization = document.getElementById('organization').value
+    let location = document.getElementById('location').value
+    let service = document.getElementById('service').value
+    let requirements = document.getElementById('requirements').value
+    let domain = window.location.origin;
+    console.log(name,email, mobile,organization,location, service,requirements, domain);
+
+    let html = `<h3>New Inquiry</h3>
+    <p>You have a new contact inquiry:</p>
+    <ul>
+        <li><strong>Name:</strong> <span>${name}</span></li>
+        <li><strong>Email:</strong> <span>${email}</span></li>
+        <li><strong>Mobile:</strong> <span>${mobile}</span></li>
+        <li><strong>Organization:</strong> <span>${organization}</span></li>
+        <li><strong>Location:</strong> <span>${location}</span></li>
+        <li><strong>Service:</strong> <span>${service}</span></li>
+        <li><strong>Requirement:</strong> <span>${requirements}</span></li>
+        <li><strong>Source:</strong> <span>${domain}</span></li>
+    </ul>
+    <p>Thank you.</p>
+    `
+
+    // let html = "<h1>New Contact</h1>"
+
+
+    let encodedHtml = encodeURIComponent(html);
+    let url = `https://gt.thelearningsetu.com/api/extra_api/contact?domain=${domain}&name=${name}&html_content=${encodedHtml}`;
+
+    alert('Thank you for your enquiry. We will get back to you soon!');
+    closeEnquiryPopup();
+    // let url1 = "https://gt.thelearningsetu.com/api/page/pages?page=1&page_size=10"
+    let res = await fetch(url);
+    let data = await res.json();
+    // console.log(data);
+
+    document.getElementById('name').value 
+    document.getElementById('email').value
+    document.getElementById('mobile').value
+    document.getElementById('organization').value
+    document.getElementById('location').value
+    document.getElementById('service').value
+    document.getElementById('requirements').value
+
+    // Add your form submission logic here
+    
+}
+
+
 // Modal functionality
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('contactModal');
